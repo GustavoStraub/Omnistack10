546d7d01c53c103e8115a25389e577807d71dfc8
Gustavo Straub <gustavostraubcovers@gmail.com>
2020-01-16 22:43:48 -0300

    expo init

---
+++ mobile/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>A gente programa o que a gente programa</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
